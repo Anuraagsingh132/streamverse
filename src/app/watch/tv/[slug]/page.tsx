3d0b2a9975d81c040060e6a4a66f4c78d2e8532e
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2025-07-31 09:35:31 +0530

    Update page.tsx

--- src/app/watch/tv/[slug]/page.tsx
+++ src/app/watch/tv/[slug]/page.tsx
@@ -14,7 +14,7 @@ export default function Page({ params }: { params: { slug: string } }) {
   const episode = searchParams.get('episode') || '1';
 
   // Construct the embed URL dynamically
-  const embedUrl = `https://embed.su/embed/tv/${id}/${season}/${episode}`;
+  //const embedUrl = `https://embed.su/embed/tv/${id}/${season}/${episode}`;
   const embedUrl = `https://vidsrc.su/tv/${id}/${season}/${episode}?autoplay=true&colour=O0ff9d&autonextepisode=true';
   
   // const embedUrl = `https://vidbinge.dev/embed/tv/${id}/${season}/${episode}`;
